39c2bc05facc50be2302f486530d2deb96481f72
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-01-02 00:55:07 +0530

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div>
+      <div className=" bg-gray-900 py-4">
+        <h2 className=" text-white text-center font-light">
+          Made with By Roshan Kumar Yadav
+        </h2>
+      </div>
+    </div>
+  );
+}
