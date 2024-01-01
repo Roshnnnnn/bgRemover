8fb9e44e4cd82135ad1e32370c277d39af3b02d9
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-01-02 00:58:02 +0530

    footer responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <div>
-      <div className=" bg-gray-900 py-4">
-        <h2 className=" text-white text-center font-light">
+      <div className="bg-gray-900 py-4">
+        <h2 className="text-white text-center text-sm lg:text-base font-light">
           Made with By Roshan Kumar Yadav
         </h2>
       </div>
